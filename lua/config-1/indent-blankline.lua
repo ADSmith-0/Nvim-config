b3ddcbb4f9a261411a0e618b5df85d95e1a816db
Adam Smith <adam00199382818@gmail.com>
2023-09-03 02:49:37 +0100

    removed rainbow colours for indent lines

--- lua/config-1/indent-blankline.lua
+++ lua/config-1/indent-blankline.lua
@@ -1,12 +1,11 @@
-local hl_list = {}
-local theme_colors = require("onedarkpro.helpers")
-local colors = theme_colors.get_colors()
-
-for i, color in pairs({ colors.yellow, colors.green, colors.purple, colors.blue }) do
-  local name = 'IndentBlanklineIndent' .. i
-  vim.api.nvim_set_hl(0, name, { fg = color })
-  table.insert(hl_list, name);
-end
-require("indent_blankline").setup {
-  char_highlight_list = hl_list,
-}
+-- local hl_list = {}
+-- local theme_colors = require("onedarkpro.helpers")
+--
+-- for i, color in pairs({ "yellow", "green", "purple", "blue" }) do
+--   local name = 'IndentBlanklineIndent' .. i
+--   vim.api.nvim_set_hl(0, name, { fg = theme_colors.darken(color, 25, "onedark") })
+--   table.insert(hl_list, name);
+-- end
+require("indent_blankline").setup({
+  -- char_highlight_list = hl_list,
+})
