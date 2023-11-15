2eadd60b1cb0a204aa9d273398eabd37e8513f90
adam smith <adam00199382818@gmail.com>
2023-11-15 23:21:43 +0000

    added expandtab and autoindent

--- lua/config-1/set.lua
+++ lua/config-1/set.lua
@@ -4,9 +4,9 @@ vim.opt.relativenumber = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-
+vim.opt.autoindent = true
 vim.opt.smartindent = true
+vim.opt.expandtab = true
 
 vim.opt.wrap = true
 
