d519a46079def4baabe0c1935060638302b89fb2
adam smith <adam00199382818@gmail.com>
2023-12-16 18:25:19 +0000

    changed leap to be bidirectional

--- lua/config-1/init.lua
+++ lua/config-1/init.lua
@@ -193,17 +193,6 @@ local plugins = {
   {
     "ggandor/leap.nvim",
     enabled = true,
-    keys = {
-      { "<leader>s", mode = { "n", "x", "o" }, desc = "Leap forward to" },
-      { "<leader>S", mode = { "n", "x", "o" }, desc = "Leap backward to" },
-    },
-    config = function(_, opts)
-      local leap = require("leap")
-      for k, v in pairs(opts) do
-        leap.opts[k] = v
-      end
-      leap.add_default_mappings(true)
-    end,
   },
   {
     "ggandor/flit.nvim",
