5f2e200d8c907442cc751dd5a129aa1edeeb3a23
adam smith <adam00199382818@gmail.com>
2023-12-26 14:07:23 +0000

    fixed useState snippet so that now the setter name gets capitalised correctly

--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -3,12 +3,14 @@ local s = ls.snippet
 local i = ls.insert_node
 local t = ls.text_node
 local fmt = require("luasnip.extras.fmt").fmt
-local rep = require("luasnip.extras").rep
+local f = ls.function_node
 
 ls.add_snippets("all", {
   s("clg", fmt("console.log({});", { i(0) })),
   s("cdr", fmt("console.dir({});", { i(0) })),
   s("cer", fmt("console.err({});", { i(0) })),
-  s("ust", fmt("const [{}, set{}] = useState<{}>({});", { i(1), rep(1), i(2), i(0) })),
+  s("ust", fmt("const [{}, set{}] = useState<{}>({});", { i(1), f(function(var_name)
+    return (string.gsub(var_name[1][1], "^%l", string.upper))
+  end, { 1 }), i(2), i(0) })),
   s("uef", fmt("useEffect(() => {{\n\t{}\n}}, [{}]);", { i(0), i(1) })),
 })
