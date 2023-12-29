ffd21e06ce60405136f42bd4acd59161d0e7d3b1
adam smith <adam00199382818@gmail.com>
2023-12-29 18:24:09 +0000

    added snippet for JS function

--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -9,6 +9,7 @@ ls.add_snippets("all", {
   s("clg", fmt("console.log({});", { i(0) })),
   s("cdr", fmt("console.dir({});", { i(0) })),
   s("cer", fmt("console.err({});", { i(0) })),
+  s("fna", fmt("const {} = ({}){} => {{\n\t{}\n}};", { i(1), i(2), i(3), i(0) })),
   s("ust", fmt("const [{}, set{}] = useState<{}>({});", { i(1), f(function(var_name)
     return (string.gsub(var_name[1][1], "^%l", string.upper))
   end, { 1 }), i(2), i(0) })),
